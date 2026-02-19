7ec16891ff77acfff8419c4722736de215435ae3
Namit Kontam <namitkontam2199@gmail.com>
2026-02-19 19:21:44 +0530

    test gameboard funtionality

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,26 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
 const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
-]
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 const GameBoard = () => {
-  return (
-    <ol id='game-board'>
-        {initialGameBoard.map((row, rowIndex) => (
-            <li key={rowIndex}>
-                <ol>
-                    {row.map((playerSymbol, colIndex) => (
-                        <li key={colIndex}>
-                            <button>{playerSymbol}</button>
-                        </li>
-                    ))}
-                </ol>
-            </li>
-        ))}
-      
-    </ol>
-  )
-}
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
-export default GameBoard
+  function handleSelect(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      const updatedGameBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedGameBoard[rowIndex][colIndex] = "X";
+      return updatedGameBoard;
+    });
+  }
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => handleSelect(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+};
+
+export default GameBoard;
