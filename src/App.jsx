53a2f571d38a5f04714e7f6a9ecdab1f40b52856
Namit Kontam <namitkontam2199@gmail.com>
2026-02-20 11:45:24 +0530

    add logic for draw

--- src/App.jsx
+++ src/App.jsx
@@ -53,6 +53,8 @@ function App() {
     }
   }
 
+  const hasDraw = gameTurns.length === 9 && !winner;
+
   function handleSelectSquare(rowIndex, colIndex) {
     // setActivePlayer((currActivePlayer) =>
     //   currActivePlayer === "X" ? "O" : "X"
@@ -82,7 +84,7 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        {winner && <GameOver winner={winner} />}
+        {(winner || hasDraw) && <GameOver winner={winner} />}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -4,7 +4,8 @@ const GameOver = ({ winner }) => {
   return (
     <div id="game-over">
       <h2>Game Over!</h2>
-      <p>{winner} won!</p>
+      {winner && <p>{winner} won!</p>}
+      {!winner && <p>Draw!</p>}
       <p>
         <button>Rematch!</button>
       </p>
